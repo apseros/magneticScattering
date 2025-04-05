70970ae400eb087c195df8f91ba990a5b5f02bf6
apseros <apseros.andreas@gmail.com>
2025-04-05 09:29:13 +0200

    Packaged example data

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, basedir)
 project = 'magneticScattering'
 copyright = '2025, Andreas Apseros'
 author = 'Andreas Apseros'
-release = '0.2.0'
+release = '0.2.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
